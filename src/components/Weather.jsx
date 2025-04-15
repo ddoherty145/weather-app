01765f1344a7984333b93d312ecf2424ae0eb9d6
David Doherty <froztydavie@Davids-MacBook-Air-5.local>
2025-04-15 00:19:19 -0700

    placeholder info

---
+++ src/components/Weather.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Weather() {
+    return (
+        <div>
+            <h1>Lester's Weather Room!</h1>
+            <p>This is the weather component.</p>
+        </div>
+    );
+}
+
+export default Weather;
